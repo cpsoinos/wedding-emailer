b78f48cdacc14a83482b764e24b2ad491fefb3a3
Corey Psoinos <coreypsoinos@gmail.com>
2017-09-03 20:07:51 -0400

    add rescue

--- app/models/response.rb
+++ app/models/response.rb
@@ -5,7 +5,10 @@ class Response < ApplicationRecord
 
   def notify_grooms
     ["alex@wesleybrandt.com", "coreypsoinos@gmail.com"].each do |groom|
-      TransportationMailer.notify_of_response(self, groom).deliver_now
+      begin
+        TransportationMailer.notify_of_response(self, groom).deliver_now
+      rescue
+      end
     end
   end
 end
